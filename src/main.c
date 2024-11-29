9ef15bece95e514a2e26c2290295d2ce7febeef1
aehrl <aehrl@student.42malaga.com>
2024-11-29 18:15:19 +0100

    added ft_last_proccess function.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aehrl <aehrl@student.42malaga.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 18:23:57 by aehrl             #+#    #+#             */
-/*   Updated: 2024/11/28 16:35:04 by aehrl            ###   ########.fr       */
+/*   Updated: 2024/11/29 15:30:43 by aehrl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,30 +121,65 @@ int	ft_child(char **cmnd, int fds[], int pipefd[], char *envp[])
 		close(fds[0]); 
 	close(pipefd[1]);
 	fds[0] = pipefd[0];
+	return (fds[0]);
+}
+
+int	ft_last_process(char **cmnd, int fds[], char *envp[])
+{
+	ft_printf("fd_out:%d\n", fds[1]);
+	//dup2(fds[1], STDOUT_FILENO);
+	if (dup2(fds[1], STDOUT_FILENO) < 0)
+	{
+		perror("dup2 failed");
+		//exit(1);
+	}
+	ft_printf("after dup2: fd_out:%d\n", fds[1]);
+	ft_printf("after dup2: cmnd:%s\n", cmnd[1]);
+	ft_printf("after dup2: envp:%s\n", envp[1]);
+	 /* if (execve(ft_get_path(envp, cmnd[0]), cmnd, envp) < 0) 
+	{
+			perror("Error executing command");
+			exit(1);
+	} */
+	/*if (fds[0] != STDIN_FILENO)
+		close(fds[0]);  */
 	return (0);
 }
 
-int ft_pipes(char *cmnd, int fds[], char *envp[])
+int ft_pipes(char *cmnd, int fds[], char *envp[], int is_last)
 {
 	pid_t	pid;
 	int		pipefd[2];
 	char	**cmd;
 
 	cmd = ft_split(cmnd, ' ');
-	if (pipe(pipefd) < 0)
-	{
-        perror("Error creating pipe");
-        return (-1);
-    }
+	if (is_last != 1 && pipe(pipefd) < 0)
+        return (perror("Error creating pipe"), -1);
 	pid = fork();
 	if (pid < 0) 
-	{
-		perror("Error forking process");
-		return (-1);
-	}
+		return (perror("Error forking process"), -1);
 	if (pid == 0)
-		ft_child(cmd, fds, pipefd, envp);
+	{
+		if (is_last != 1)
+		{
+			close(pipefd[0]);
+			ft_child(cmd, fds, pipefd, envp);
+		}
+		else
+			ft_last_process(cmd, fds, envp);
+	}
+	if (is_last != 1)
+	{
+        close(pipefd[1]);
+        close(fds[0]);
+        fds[0] = pipefd[0];
+    }
+	ft_printf("fd_out:%d\n", fds[1]);
+	close(fds[1]);
+	ft_printf("STDOUT:%d\n",  STDOUT_FILENO);
 	waitpid(pid, NULL, 0);
+	if (is_last == 1)
+		return (-1);
 	return (fds[0]);
 }
 
@@ -183,13 +218,16 @@ int ft_pipes(char *cmnd, int fds[], char *envp[])
 	close(fd_out);
 	return (0);
 } */
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	int		count;
 	int		fds[2];
 	int		fd_temp;
+	int		is_last;
 	
 	count = 2;
+	is_last = 0;
 	if (argc < 5)
 		return (argv = NULL, ft_printf("Wrong argument count"), -1);
 	if (ft_strncmp(argv[count - 1], "here_doc", ft_strlen(argv[count - 1])) == 0)
@@ -202,12 +240,17 @@ int	main(int argc, char *argv[], char *envp[])
 	envp = ft_get_environment(envp);
 	while (count < argc - 1)
 	{
-		fd_temp = ft_pipes(argv[count], fds, envp);
-		count++;
+		if (count == argc - 2)
+			is_last = 1;
+		fd_temp = ft_pipes(argv[count], fds, envp, is_last);
+		ft_printf("fd_temp:%d\n", fd_temp);
+		if (fd_temp < 0)
+		{
+			close(fds[1]);
+			return (0);
+		}
 		fds[1] = fd_temp;
-		//fd_in = fd_temp;
-	/* 	if (count == argc -1)
-			ft_last_pipe(); */
+		count++;
 	}
 	return (0);
 }
